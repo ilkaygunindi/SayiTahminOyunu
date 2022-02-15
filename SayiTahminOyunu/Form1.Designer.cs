2b29916f92b4a1d896049d44b73ada81d4df87c5
ilkaygunindi <90377712+ilkaygunindi@users.noreply.github.com>
2022-02-15 21:58:25 +0300

    Add project files.

---
+++ SayiTahminOyunu/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace SayiTahminOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblBaslik = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lblKalanCan = new System.Windows.Forms.Label();
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.AutoSize = true;
+            this.lblBaslik.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBaslik.Location = new System.Drawing.Point(436, 59);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(315, 37);
+            this.lblBaslik.TabIndex = 0;
+            this.lblBaslik.Text = "Sayı Tahmin Oyunu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(474, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tahmininizi Giriniz :";
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Location = new System.Drawing.Point(533, 197);
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(117, 30);
+            this.txtTahmin.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lblKalanCan
+            // 
+            this.lblKalanCan.AutoSize = true;
+            this.lblKalanCan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKalanCan.Location = new System.Drawing.Point(500, 249);
+            this.lblKalanCan.Name = "lblKalanCan";
+            this.lblKalanCan.Size = new System.Drawing.Size(146, 29);
+            this.lblKalanCan.TabIndex = 4;
+            this.lblKalanCan.Text = "Kalan Can: ";
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTahmin.Location = new System.Drawing.Point(505, 294);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(186, 41);
+            this.btnTahmin.TabIndex = 5;
+            this.btnTahmin.Text = "Tahmin Et!";
+            this.btnTahmin.UseVisualStyleBackColor = true;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1426, 1400);
+            this.Controls.Add(this.btnTahmin);
+            this.Controls.Add(this.lblKalanCan);
+            this.Controls.Add(this.txtTahmin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblBaslik);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBaslik;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTahmin;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label lblKalanCan;
+        private System.Windows.Forms.Button btnTahmin;
+    }
+}
+
---
+++ SayiTahminOyunu/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SayiTahminOyunu
+{
+    public partial class Form1 : Form
+    {
+
+        int tahminEdilecekSayi = (new Random()).Next(0, 100);
+
+        int can = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public String kalanCan(int canDegeri)
+        {
+            string canYazisi = "";
+
+            if (canDegeri == 4)
+            {
+                canYazisi = "❤❤❤❤✘";
+            }
+            else if (canDegeri == 3)
+            {
+                canYazisi = "❤❤❤✘✘";
+            }
+            else if (canDegeri == 2)
+            {
+                canYazisi = "❤❤✘✘✘";
+            }
+            else if (canDegeri == 1)
+            {
+                canYazisi = "❤✘✘✘✘";
+            }
+            else if (canDegeri == 0)
+            {
+                canYazisi = "✘✘✘✘✘";
+            }
+            return canYazisi;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTahmin_Click(object sender, EventArgs e)
+        {
+            if (can == 0)
+            {
+                MessageBox.Show("Maalesef canınız kalmadı" + can, "Üzgünüz!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            if (txtTahmin.Text == null)
+            {
+                MessageBox.Show("Lütfen bir değer giriniz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            int tahmin = Convert.ToInt32(txtTahmin.Text);
+
+            can--;
+
+            lblKalanCan.Text = kalanCan(can);
+
+            if (tahmin == tahminEdilecekSayi)
+            {
+                if (MessageBox.Show("Tahmininiz Doğru! Yeni Oyuna Başlamak İster misiniz?", "!!Tebrikler!!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Application.Restart();
+                }
+                else
+                {
+                    MessageBox.Show("Uygulama Kapatılıyor.", "Kapatılıyor.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+            else
+            {
+                if (can == 0)
+                {
+                    MessageBox.Show("Oyun Bitti! Bulmanız gereken sayı: " + tahminEdilecekSayi, "Üzgünüz.", MessageBoxButtons.YesNo, MessageBoxIcon.Hand);
+                    if (MessageBox.Show("Yeni Oyuna Başlamak İster misiniz?", "Bİr El Daha :)", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        Application.Restart();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tekrar Görüşmek Üzere. Uygulama Kapatılıyor.", "Kapatılıyor.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Application.Exit();
+                    }
+                }
+                if (tahminEdilecekSayi > tahmin)
+                {
+                    MessageBox.Show("Tahmininiz Yanlış. Tahmin Değerinizi Büyütün \n Kalan Canınız: " + can, "Üzgünüz!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Tahmininiz Yanlış! Tahmin Değerinizi Küçültün \n Kalan canınız: " + can, "Üzgünüz!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
